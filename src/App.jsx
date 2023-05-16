29733222b383fc1527fde5a3be65e1d06d02c480
Houssein Djirdeh <houssein@google.com>
2023-05-16 13:36:19 -0400

    adds tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
-import styles from './App.module.css';
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+    <div class="bg-gray">
         <a
-          class={styles.link}
+          class="text-3xl font-bold underline"
           href="https://github.com/solidjs/solid"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn Solid
         </a>
-      </header>
     </div>
   );
 }
